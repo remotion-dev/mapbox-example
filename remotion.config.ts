21533d2cd53f20086de2f263fa51801e96959cdf
Jonny Burger <jonathanburger11@gmail.com>
2024-01-08 09:33:38 +0100

    V4

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,6 +1,6 @@
-import {Config} from 'remotion';
+import {Config} from '@remotion/cli/config';
 
-Config.setImageFormat('jpeg');
+Config.setVideoImageFormat('jpeg');
 Config.setOverwriteOutput(true);
 // Set this to swiftshader when rendering on Lambda
 Config.setChromiumOpenGlRenderer('angle');
